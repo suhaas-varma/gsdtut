e217d08ff449bf7173ae37b1ddceb41fb8a54b80
suhaas varma <suhaas@Ramalingas-MacBook.local>
2020-05-16 14:55:51 +0530

    Updates

---
+++ precache-manifest.b3cff5b0f4b0e068087d0678d2734c2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdfe6ac68990a3da01eb0305838b7a09",
+    "url": "/gsdtut/index.html"
+  },
+  {
+    "revision": "c81a312e33aae5aa9eba",
+    "url": "/gsdtut/static/css/main.fde51515.chunk.css"
+  },
+  {
+    "revision": "ceb2ce5d4704d640ae73",
+    "url": "/gsdtut/static/js/2.940f58dc.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/gsdtut/static/js/2.940f58dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c81a312e33aae5aa9eba",
+    "url": "/gsdtut/static/js/main.0f8e7cc9.chunk.js"
+  },
+  {
+    "revision": "4432474e6827b2e58406",
+    "url": "/gsdtut/static/js/runtime-main.9a12eb7a.js"
+  }
+]);
